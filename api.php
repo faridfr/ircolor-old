f8d6328eacbfd483b3645ba04e1d0b2afb0d8a05
Farid Froozan <froozan@yahoo.com>
2018-11-27 00:05:42 +0330

    some changes in api

--- api.php
+++ api.php
@@ -31,7 +31,7 @@ if(isset($_GET['hex']) && $_GET['hex']!='')
 	else if($color->isDark( $input ))
 	{
 		$dl = 'dark';
-		$response['status'] = 'dark';
+		$response['color_status'] = 'dark';
 		$response['text_color'] = 'white';
 	}
 
@@ -47,16 +47,17 @@ if(isset($_GET['hex']) && $_GET['hex']!='')
 	$response['darker'] = array();
 	$response['brighter'] = array();
 
-	for($i=0;$i<10;$i++)
+	for($i=0;$i<18;$i++)
 	{	
-		$newcolor = $color->darken('2');
+		$newcolor = $color->darken('4');
 		$color = new Color($newcolor);
 		$dark_color  = $color->gethex();
 		array_push($response['darker'], $dark_color);
 	}
-	for($i=0;$i<10;$i++)
+	$color = new Color($input);
+	for($i=0;$i<18;$i++)
 	{
-		$newcolor = $color->lighten('2');
+		$newcolor = $color->lighten('4');
 		$color = new Color($newcolor);
 		$light_color  = $color->gethex();
 		array_push($response['brighter'], $light_color);
@@ -68,32 +69,38 @@ else if(isset($_GET['spectrum']) && $_GET['spectrum']!='')
 {
 	$response['colors'] = array();
 
+
 	switch ($_GET['spectrum']) {
 		case 'random':
-			foreach (RandomColor::many(27) as $c){
-				array_push($response['colors'], substr($c, -6));
-			}
-			break;
+		$response['spectrum'] = 'Random';
+		foreach (RandomColor::many(27) as $c){
+			array_push($response['colors'], substr($c, -6));
+		}
+		break;
 
 		case 'red':
+		$response['spectrum'] = 'Red';
 		foreach (RandomColor::many(27, array('hue'=>'red')) as $c){
 			array_push($response['colors'], substr($c, -6));
 		}
 		break;
 
 		case 'green':
+		$response['spectrum'] = 'Green';
 		foreach (RandomColor::many(27, array('hue'=>'green')) as $c){
 			array_push($response['colors'], substr($c, -6));
 		}
 		break;
 
 		case 'blue':
+		$response['spectrum'] = 'Blue';
 		foreach (RandomColor::many(27, array('hue'=>'blue')) as $c){
 			array_push($response['colors'], substr($c, -6));
 		}
 		break;
 		
 		case 'yellow':
+		$response['spectrum'] = 'Yellow';
 		foreach (RandomColor::many(27, array('hue'=>'yellow')) as $c){
 			array_push($response['colors'], substr($c, -6));
 		}
